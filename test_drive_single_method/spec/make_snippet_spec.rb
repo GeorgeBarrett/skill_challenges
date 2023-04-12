25aa20fd2ff78dcdc50b8209671363723254f03f
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-12 17:20:16 +0100

    test suite complete for ellipsis programme 'make_snippet.rb' in lib folder

--- test_drive_single_method/spec/make_snippet_spec.rb
+++ test_drive_single_method/spec/make_snippet_spec.rb
@@ -7,11 +7,5 @@ RSpec.describe "make_snippet method" do
     result = make_snippet("this is more than five words")
     expect(result).to eq "this is more than five..."
   end
-
-  it  "if the string is shorter than five words" do
-
-    result = make_snippet("obelisk density")
-    expect(result).to eq "obelisk density"
-  end
 end
     
