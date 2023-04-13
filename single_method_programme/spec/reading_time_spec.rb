cf2fad36cd5d5f7c1d147b7d556a2e28ce9ce2f0
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-13 13:31:04 +0100

    amendments to test suite

--- single_method_programme/spec/reading_time_spec.rb
+++ single_method_programme/spec/reading_time_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe "reading_time method" do
 
   it "a method that returns an estimated reading time, assuming 200 words are read per minute" do
 
-  result = reading_time("blah blah blah blah blah blah blah")
-  expect(result).to eq 0.035
+  # result = reading_time("blah blah blah blah blah blah blah")
+  # expect(result).to eq 0.035
 
-  result = reading_time("blah blah")
-  expect(result).to eq 0.01
+  # result = reading_time("blah blah")
+  # expect(result).to eq 0.01
+
+  result = reading_time("blah")
+  expect(result). to eq 0.3
 
   end
 end
