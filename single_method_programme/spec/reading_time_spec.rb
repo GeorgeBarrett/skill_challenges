47b8a7267992b5e6141be36a7ffb8ad6d263db95
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-13 11:37:02 +0100

    initial test suite compiled. testing RED

---
+++ single_method_programme/spec/reading_time_spec.rb
@@ -0,0 +1,14 @@
+require "reading_time.rb"
+
+RSpec.describe "reading_time method" do
+
+  it "a method that returns an estimated reading time, assuming 200 words are read per minute" do
+
+  result = reading_time("blah blah blah blah blah blah blah")
+  expect(result).to eq 0.035
+
+  result = reading_time("blah blah")
+  expect(result).to eq 0.01
+
+  end
+end
