5c2cdc6b71e97ab3fa03ecf01ec5f977753e723c
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-13 17:10:53 +0100

    test suite complete for grammar_checker.rb in lib folder. all tests passing

--- single_method_programme/spec/grammar_checker_spec.rb
+++ single_method_programme/spec/grammar_checker_spec.rb
@@ -2,11 +2,60 @@ require "grammar_checker.rb"
 
 RSpec.describe "grammar_checker method" do
 
-  it "string does not start with a capital or end with full stop" do
+  it "fails" do
 
-    result = grammar_checker("this is wrong")
+    expect { grammar_checker("") }.to raise_error "You idiot"
+
+  end
+
+  # it "string ends with an appropriate punctuation mark"
+
+  #   result = grammar_checker("word!")
+  #   expect(result).to eq true
+  
+  # end
+  
+  it "if string has capital and ends with full stop" do
+
+    result = grammar_checker("Capital.")
+    expect(result).to eq true
+
+  end
+
+  it "if string has capital but no full stop" do
+
+    result = grammar_checker("Capital")
     expect(result).to eq false
 
   end
+
+  it "if string has full stop but no capital" do
+
+    result = grammar_checker("capital.")
+    expect(result).to eq false
+
+  end
+
+  it "if string has neither capital or punctuation" do
+
+    result = grammar_checker("capital")
+    expect(result).to eq false
+
+  end
+
+  it "if string has capital and exclamation mark" do
+
+    result = grammar_checker("Capital!")
+    expect(result).to eq true
+
+  end
+
+  it "if string has capital but no question mark" do
+
+    result = grammar_checker("Capital?")
+    expect(result).to eq true
+
+  end
+
 end
 
