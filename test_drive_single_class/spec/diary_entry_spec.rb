1f7d395d6cfea08c43f830b94d884865a05bce8c
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-17 10:30:05 +0100

    two tests written in suite. Both testing GREEN

---
+++ test_drive_single_class/spec/diary_entry_spec.rb
@@ -0,0 +1,17 @@
+require "diary_entry.rb"
+
+RSpec.describe DiaryEntry do
+  it "constructs" do 
+    diary_entry = DiaryEntry.new("my_title", "my_contents")
+    expect(diary_entry.title).to eq "my_title"
+    expect(diary_entry.contents).to eq "my_contents"
+  end
+
+  describe "#count_words" do
+    it "returns the number of words inn contents" do
+      diary_entry = DiaryEntry.new("my_title", "some contents here")
+      expect(diary_entry.count_words).to eq 3
+    end
+  end
+
+end
