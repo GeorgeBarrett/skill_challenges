679773c8c82837e8cfc1eaac43e9d9e9abcc1cf7
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-17 15:40:18 +0100

    initialise method created with its instance variables. 'check' method altered to make use of the counters defined in 'initialise' method. 'percentage_good' method started but only works for strings that pass the checks

--- test_drive_single_class/lib/grammar_stats.rb
+++ test_drive_single_class/lib/grammar_stats.rb
@@ -1,24 +1,30 @@
 class GrammarStats
 
   def initialize
-    #...
+    @total_checks = 0
+    @passed_checks = 0
+    @failed_checks = 0
   end
 
   def check(text) # text is a string
     fail "Please enter some words." if text.empty?
-
+    @total_checks += 1
     punctuation = [".", "!", "?"]
     first_character = text.chars.first
     last_character = text.chars.last
 
     if first_character == first_character.upcase && punctuation.include?(last_character)
+      @passed_checks += 1
       return true
     else
+      @failed_checks += 1
       return false
     end
   end
 
   def percentage_good
+    (@passed_checks / @total_checks) * 100 
+  
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
   end
