b857e0f66120eebf64112efdef6cf025367f9674
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-17 10:48:18 +0100

    test written for reading_time method. testing GREEN

--- test_drive_single_class/spec/diary_entry_spec.rb
+++ test_drive_single_class/spec/diary_entry_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe DiaryEntry do
       diary_entry = DiaryEntry.new("my_title", "some contents here")
       expect(diary_entry.count_words).to eq 3
     end
+
+    it "returns zero when contents is empty" do
+      diary_entry = DiaryEntry.new("my_title", "")
+      expect(diary_entry.count_words).to eq 0
+    end
+  end
+
+  describe "#reading_time" do 
+    context "given a sensible number of wpm (200)" do
+      it "returns the ceiling of the number of minutes it takes to read the context" do
+        diary_entry = DiaryEntry.new("my_title", "one " * 550)
+        expect(diary_entry.reading_time(200)).to eq 3
+      end
+    end
   end
 
 end
