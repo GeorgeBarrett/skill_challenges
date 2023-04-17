8c2cbb44adbe9bdc4949c2f89811582cbb55db69
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-17 13:49:16 +0100

    reading_chunk method complete

--- test_drive_single_class/lib/diary_entry.rb
+++ test_drive_single_class/lib/diary_entry.rb
@@ -23,20 +23,17 @@ class DiaryEntry
   end
 
   def reading_chunk(wpm, minutes)
+    fail "Reading speed must be above zero." unless wpm > 0
     no_words_we_can_read = wpm * minutes
     start_from = @furthest_word_read
     end_at = @furthest_word_read + no_words_we_can_read
     word_list = words[start_from, end_at]
-    @furthest_word_read = end_at
+    if end_at >= count_words
+      @furthest_word_read = 0
+    else
+      @furthest_word_read = end_at
+    end
     return word_list.join(" ")
-    
-    # `wpm` is an integer representing the number of words the user can read per minute
-    # `minutes` is an integer representing the number of minutes the user has to read
-    # Returns a string with a chunk of the contents that the user could read
-    # in the given number of minutes.
-    # If called again, `reading_chunk` should return the next chunk, skipping
-    # what has already been read, until the contents is fully read.
-    # The next call after that it should restart from the beginning.
   end
 
   private
