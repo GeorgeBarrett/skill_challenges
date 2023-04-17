103b2a83592ed4b1698c827789cbe7a49d8d97c8
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-17 13:50:45 +0100

    test cases written for reading_chunk method. test suite complete. all tests running GREEN

--- test_drive_single_class/spec/diary_entry_spec.rb
+++ test_drive_single_class/spec/diary_entry_spec.rb
@@ -33,7 +33,54 @@ RSpec.describe DiaryEntry do
         expect { diary_entry.reading_time(0) }.to raise_error "Reading speed must be above zero." 
       end
     end
-
   end
 
+  describe "#reading_chunk" do
+    context "with a contents readable within the time" do 
+      it "returns the full contents" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        chunk = diary_entry.reading_chunk(200, 1)
+        expect(chunk).to eq "one two three"
+      end
+    end
+
+    context "given a wpm of 0" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        expect { diary_entry.reading_chunk(0, 5) }.to raise_error "Reading speed must be above zero." 
+      end
+    end
+
+    context "with a contents unredable within the time" do
+      it "returns a readbale chunk" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+      it "returns the next chunk, the next time we are asked" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "three"
+      end
+
+      it "restarts after reading the whole content" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(2, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+
+      it "restarts if it finishes exactly on the end" do
+        diary_entry = DiaryEntry.new("my_title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(1, 1)
+        chunk = diary_entry.reading_chunk(2, 1)
+        expect(chunk).to eq "one two"
+      end
+      
+    end
+  end
 end
