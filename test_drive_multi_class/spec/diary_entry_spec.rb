5d7569a7bbfecb5e08b03f69fa728b43e0695c45
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-18 17:32:56 +0100

    second test written to count the words in contents. running RED

--- test_drive_multi_class/spec/diary_entry_spec.rb
+++ test_drive_multi_class/spec/diary_entry_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe DiaryEntry do
     expect(diary_entry.title).to eq "my_title"
     expect(diary_entry.contents).to eq "my_contents"
   end
+
+  describe "#count_words" do
+    it "counts words in contents" do
+      diary_entry = DiaryEntry.new("my_title", "one two three four")
+      expect(diary_entry.count_words).to eq 4
+    end
+  end
 end
