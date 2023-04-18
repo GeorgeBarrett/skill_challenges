5054512e2c0034947c368264910b8098a8d7f8d9
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-18 11:12:30 +0100

    all tests passing GREEN

--- single_class_programme/spec/track_list_spec.rb
+++ single_class_programme/spec/track_list_spec.rb
@@ -1,40 +1,28 @@
-# track_list = TrackList.new()
-
-# track_list.list # => track_list = []
-
-# track_list.list # => if list is empty then a fail message will be thrown
-
-# track_list = TrackList.new()
-# track_list.add("Poliwhirl")
-# track_list.list # => ["Poliwhirl"]
-
-# track_list = TrackList.new()
-# track_list.add("Vicarious")
-# track_list.add("Jambi")
-# track_list.list # => ["Vicarious", "Jambi"]
-
-require "track_list"
+require "track_list.rb"
 
 RSpec.describe TrackList do
   
   context "if list is empty" do
     it "it fails" do
-      expect {todo_list.list("") }.to raise_error "Track list is empty."
+      track_list = TrackList.new
+      expect { track_list.list }.to raise_error "Track list is empty."
     end
   end
 
   context "give a track" do
-    xit "add track to track list" do
-      todo_list.add("Poliwhirl")
+    it "add track to track list" do
+      track_list = TrackList.new
+      track_list.add("Poliwhirl")
       expect(track_list.list).to eq ["Poliwhirl"]
     end
   end
 
   context "give multiple tracks" do
-    xit "add two tracks to the list" do
-      todo_list.add("Poliwhirl")
-      todo_list.add("Jambi")
-      todo_list.add("This must be the place")
+    it "add two tracks to the list" do
+      track_list = TrackList.new
+      track_list.add("Poliwhirl")
+      track_list.add("Jambi")
+      track_list.add("This must be the place")
       expect(track_list.list).to eq ["Poliwhirl", "Jambi", "This must be the place"]  
     end
   end
