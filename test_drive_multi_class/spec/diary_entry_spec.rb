472c554dfdf6212e53da215b88303c206f1a8ec9
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-18 17:44:17 +0100

    test added so zero returns if contents is empty. Running GREEN

--- test_drive_multi_class/spec/diary_entry_spec.rb
+++ test_drive_multi_class/spec/diary_entry_spec.rb
@@ -8,9 +8,15 @@ RSpec.describe DiaryEntry do
   end
 
   describe "#count_words" do
-    it "counts words in contents" do
+    it "returns zero if contents is empty" do
+      diary_entry = DiaryEntry.new("my_title", "")
+      expect(diary_entry.count_words).to eq 0
+    end
+
+    it "counts the words in the contents" do
       diary_entry = DiaryEntry.new("my_title", "one two three four")
       expect(diary_entry.count_words).to eq 4
     end
+
   end
 end
