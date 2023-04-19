3012c7697deaf7e07529ca7329af0505e67419b2
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-19 10:56:09 +0100

    'reading_time' method constructed. all tests running GREEN

--- test_drive_multi_class/lib/diary.rb
+++ test_drive_multi_class/lib/diary.rb
@@ -16,10 +16,10 @@ class Diary
     return @entries.sum(&:count_words)
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # if the user were to read all entries in the diary.
+  def reading_time(wpm) 
+    return @entries.sum do |entry|
+      entry.reading_time(wpm)
+    end
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
