fcc328b8f659ec396664132f73d591c5a24003a1
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-19 15:42:01 +0100

    test for returning multiple imcomplete tasks. running GREEN

--- test_drive_multi_class/challenge/spec/todo_integration_spec.rb
+++ test_drive_multi_class/challenge/spec/todo_integration_spec.rb
@@ -7,11 +7,17 @@ RSpec.describe TodoList do
       todo_list = TodoList.new
       todo = Todo.new('finish multitask classes')
       todo_list.add(todo)
-
       expect(todo_list.incomplete).to eq [todo]
     end
 
-    
+    it "returns list of two incomplete tasks" do
+      todo_list = TodoList.new
+      todo = Todo.new('lalala')
+      todo_2 = Todo.new('blah blah')
+      todo_list.add(todo)
+      todo_list.add(todo_2)
+      expect(todo_list.incomplete).to eq [todo, todo_2] 
+    end
   
     xit "raise error if todo already exists" do
       todo_list = TodoList()
