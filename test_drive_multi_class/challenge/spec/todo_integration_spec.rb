5ca4578c43c24d36f65f1ed767e40f5df2991d63
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-19 15:10:14 +0100

    tdd approach. add method working. test running GREEN

--- test_drive_multi_class/challenge/spec/todo_integration_spec.rb
+++ test_drive_multi_class/challenge/spec/todo_integration_spec.rb
@@ -1,25 +1,23 @@
-require "todo"
-require "todo_list"
-
 require 'todo'
 require 'todo_list'
 
 RSpec.describe TodoList do
   context 'calls from Todo' do 
-    xit 'adds a todo to the todo list' do
+    it 'adds a todo to the todo list' do
       todo_list = TodoList.new
       todo = Todo.new('finish multitask classes')
-      result = todo_list.add(todo)
+      todo_list.add(todo)
 
-      expect(result.incomplete).to eq 'finish multitask classes'
+      expect(todo_list.incomplete).to eq [todo]
     end
+
+    
   
-    it "raise error if todo already exists" do
+    xit "raise error if todo already exists" do
       todo_list = TodoList()
       todo = Todo.new('something')
       todo_2 = Todo.new('something')
       expect { todo_2 }.to raise_error "Todo already exists."
     end
-
   end
 end
