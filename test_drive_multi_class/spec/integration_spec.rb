63f7e2c9d83b8bb5498c0e73ec4d9c5fa9e9e364
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-19 10:23:00 +0100

    typo. word counting behaviour test now running GREEN

--- test_drive_multi_class/spec/integration_spec.rb
+++ test_drive_multi_class/spec/integration_spec.rb
@@ -2,25 +2,35 @@ require "diary"
 require "diary_entry"
 
 RSpec.describe "integration" do
-  context "after adding some entries" do
-    it "lists out the entries added" do
+  it "lists out the entries added" do
+    diary = Diary.new
+    diary_entry_1 = DiaryEntry.new("my_title", "my_contents")
+    diary_entry_2 = DiaryEntry.new("my_title 2", "my_contents 2")
+    diary.add(diary_entry_1)
+    diary.add(diary_entry_2)
+    expect(diary.all).to eq [diary_entry_1, diary_entry_2]
+  end
+    
+  describe "word counting behaviour" do
+    it "counts the words in all diaries entries' contents" do
       diary = Diary.new
-      diary_entry_1 = DiaryEntry.new("my_title", "my_contents")
-      diary_entry_2 = DiaryEntry.new("my_title 2", "my_contents 2")
+      diary_entry_1 = DiaryEntry.new("my title", "my contents")
+      diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
       diary.add(diary_entry_1)
       diary.add(diary_entry_2)
-      expect(diary.all).to eq [diary_entry_1, diary_entry_2]
-    end
-    
-    describe "#count_words" do
-      xit "counts the words in all diaries entries' contents" do
-        diary = Diary.new
-        diary_entry_1 = DiaryEntry.new("my_title", "my_contents")
-        diary_entry_2 = DiaryEntry.new("my_title 2", "my_contents 2")
-        diary.add(diary_entry_1)
-        diary.add(diary_entry_2)
-        expect(diary.count_words).to eq 5
-      end
+      expect(diary.count_words).to eq 5
     end
   end
+
+  describe "reading time behaviour" do
+    it "calculates the reading time for all entries" do
+      diary = Diary.new
+      diary_entry_1 = DiaryEntry.new("my title", "my contents")
+      diary_entry_2 = DiaryEntry.new("my title 2", "my contents 2")
+      diary.add(diary_entry_1)
+      diary.add(diary_entry_2)
+      expect(diary.reading_time(2)).to eq 3
+    end
+  end
+
 end
