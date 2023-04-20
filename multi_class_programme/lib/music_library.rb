114d44be156e48e295a04b43e3eb1a4227506a99
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-20 09:27:37 +0100

    programme template complete

---
+++ multi_class_programme/lib/music_library.rb
@@ -0,0 +1,27 @@
+class MusicLibrary
+  def initialize
+    # ...
+  end
+
+  def add(track) # track is an instance of Track
+    # Track gets added to the library
+    # Returns nothing
+  end
+
+  def all
+    # Returns a list of track objects
+  end
+  
+  def search_by_title(keyword) # keyword is a string
+    # Returns a list of tracks with titles that include the keyword
+  end
+end
+
+class Track
+  def initialize(title, artist) # title and artist are both strings
+  end
+
+  def format
+    # Returns a string of the form "TITLE by ARTIST"
+  end
+end
