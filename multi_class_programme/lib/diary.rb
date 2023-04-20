75bfcb0bb1860776c37391624e0db52ea2ecabaf
George Barrett <geab@MASTUDENT-George-Barrett-C02G5GZZQ6L4.local>
2023-04-20 11:35:06 +0100

    class templates complete

---
+++ multi_class_programme/lib/diary.rb
@@ -0,0 +1,22 @@
+class Diary
+  def initialize
+    @entries = []
+    @mobile_numbers = []
+  end
+
+  def add_entry(entry) 
+  end
+
+  def add_todo(todo)
+
+  def list_entries
+    @entries
+  end
+
+  def diary_entries_based_speed(minutes, wpm)
+  end
+  
+  def list_mobile_numbers
+    # if an entry is 11 numbers then throw into mobile_numbers array
+  end
+end
